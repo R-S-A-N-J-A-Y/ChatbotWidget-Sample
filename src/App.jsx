726f40c8fc80eaaa065ed63d5669b528528adf1a
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-08-07 12:07:45 +0530

    implemented the styles via tailwind and updated the ui of the chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
-import "./App.css"
+import { RxCross1 } from "react-icons/rx";
+import { TbMessageChatbot } from "react-icons/tb";
 
 const App = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="chatbot-container">
+    <div className="absolute bottom-10 right-10 flex flex-col items-end gap-2">
       {open && (
-        <div className="chatbot-window">
+        <div className="bg-gray-200 p-5 w-100 h-100 rounded-2xl">
           <h3>Hello! I’m your assistant</h3>
           <p>Ask me anything.</p>
         </div>
       )}
-      <button className="chatbot-toggle" onClick={() => setOpen(!open)}>
-        💬
+      <button
+        className="p-2 h-15 w-15 flex items-center justify-center cursor-pointer bg-blue-700 rounded-xl outline-none"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? (
+          <RxCross1 color="white" size={25} />
+        ) : (
+          <TbMessageChatbot color="white" size={35} />
+        )}
       </button>
     </div>
   );
